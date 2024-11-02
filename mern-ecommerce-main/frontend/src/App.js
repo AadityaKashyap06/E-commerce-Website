a54aeec98f441c361c3b1dc0b946716b3518b2f4
AADITYA KASHYAP <adityakash2806@gmail.com>
2024-11-02 23:02:08 +0530

    ecom

--- mern-ecommerce-main/frontend/src/App.js
+++ mern-ecommerce-main/frontend/src/App.js
@@ -1,71 +1,24 @@
-import { useSelector } from 'react-redux';
-import {
-  Navigate,
-  Route, RouterProvider, createBrowserRouter, createRoutesFromElements
-} from "react-router-dom";
-import { selectIsAuthChecked, selectLoggedInUser } from './features/auth/AuthSlice';
-import { Logout } from './features/auth/components/Logout';
-import { Protected } from './features/auth/components/Protected';
-import { useAuthCheck } from "./hooks/useAuth/useAuthCheck";
-import { useFetchLoggedInUserDetails } from "./hooks/useAuth/useFetchLoggedInUserDetails";
-import { AddProductPage, AdminOrdersPage, CartPage, CheckoutPage, ForgotPasswordPage, HomePage, LoginPage, OrderSuccessPage, OtpVerificationPage, ProductDetailsPage, ProductUpdatePage, ResetPasswordPage, SignupPage, UserOrdersPage, UserProfilePage, WishlistPage } from './pages';
-import { AdminDashboardPage } from './pages/AdminDashboardPage';
-import { NotFoundPage } from './pages/NotFoundPage';
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-
-function App() {
-
-  const isAuthChecked=useSelector(selectIsAuthChecked)
-  const loggedInUser=useSelector(selectLoggedInUser)
-
-
-  useAuthCheck();
-  useFetchLoggedInUserDetails(loggedInUser);
-
-
-  const routes = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path='/signup' element={<SignupPage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/verify-otp' element={<OtpVerificationPage/>}/>
-        <Route path='/forgot-password' element={<ForgotPasswordPage/>}/>
-        <Route path='/reset-password/:userId/:passwordResetToken' element={<ResetPasswordPage/>}/>
-        <Route exact path='/logout' element={<Protected><Logout/></Protected>}/>
-        <Route exact path='/product-details/:id' element={<Protected><ProductDetailsPage/></Protected>}/>
-
-        {
-          loggedInUser?.isAdmin?(
-            // admin routes
-            <>
-            <Route path='/admin/dashboard' element={<Protected><AdminDashboardPage/></Protected>}/>
-            <Route path='/admin/product-update/:id' element={<Protected><ProductUpdatePage/></Protected>}/>
-            <Route path='/admin/add-product' element={<Protected><AddProductPage/></Protected>}/>
-            <Route path='/admin/orders'  element={<Protected><AdminOrdersPage/></Protected>}/>
-            <Route path='*' element={<Navigate to={'/admin/dashboard'}/>}/>
-            </>
-          ):(
-            // user routes
-            <>
-            <Route path='/' element={<Protected><HomePage/></Protected>}/>
-            <Route path='/cart' element={<Protected><CartPage/></Protected>}/>
-            <Route path='/profile' element={<Protected><UserProfilePage/></Protected>}/>
-            <Route path='/checkout' element={<Protected><CheckoutPage/></Protected>}/>
-            <Route path='/order-success/:id' element={<Protected><OrderSuccessPage/></Protected>}/>
-            <Route path='/orders' element={<Protected><UserOrdersPage/></Protected>}/>
-            <Route path='/wishlist' element={<Protected><WishlistPage/></Protected>}/>
-            </>
-          )
-        }
-
-        <Route path='*' element={<NotFoundPage/>} />
-
-      </>
-    )
-  )
-
-  
-  return isAuthChecked ? <RouterProvider router={routes}/> : "";
-}
+const App = () => {
+  return (
+    <div className='position-relative'>
+      <Header />
+      <main>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+      <ToastContainer autoClose={1000} />
+    </div>
+  );
+};
 
 export default App;
