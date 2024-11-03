edf6db295f6e0ee38b9a1c06b69547ff0cc85cdd
AADITYA KASHYAP <adityakash2806@gmail.com>
2024-11-03 12:08:39 +0530

    ecom_web

--- MERN-eCommerce-Website-master/backend/utils/generateTokens.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-const generateToken = (id) =>
-{
-    return jwt.sign({ id } , process.env.JWT_SECRET , { expiresIn: '30d'}) // payload is ID , JWT_SECRET from .env , Token expiresin 30 days
-}
-
-module.exports = generateToken
